34a919df2721df95cc0fa421b9bd20ab55aca020
Christopher (Ted) Burns <ceburns111@gmail.com>
2022-07-12 13:12:10 -0400

    feat:finished adding refresh token functionality

--- Gassy/Entities/User.cs
+++ Gassy/Entities/User.cs
@@ -11,7 +11,6 @@ public class User
     public string FirstName { get; set; }
     public string LastName { get; set;}
     public string Email { get; set; }
-    public string UserName { get; set; }
     [JsonIgnore]
     public string PasswordHash { get; set; }
     public RoleId RoleId { get; set; }
